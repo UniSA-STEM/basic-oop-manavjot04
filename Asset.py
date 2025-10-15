009e3dedfcb0e69e4d3000b3bc03526a81eaae72
manavjot04 <dutmy005@mymail.unisa.edu.au>
2025-10-15 17:55:56 +1030

    Started with the formation of the class asset and movig onto the making of the subclasses.

--- Asset.py
+++ Asset.py
@@ -1,8 +1,32 @@
 """
 File: Asset.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Manavjot Singh Dutta
+ID: 110430330
+Username: Dutmy005
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from abc import ABC, abstractmethod
+
+class Asset(ABC):
+    
+    def __init__(self, name, description, encrypted=False): #For initializing an asset 
+        self._name = name
+        self._description = description
+        self._encrypted = encrypted
+
+    @property
+    def name(self): #This is for name of the asset
+        return self._name
+
+    @property
+    def encrypted(self): #this for keeping the asset encrypted
+        return self._encrypted
+
+    @abstractmethod
+    def __str__(self):
+        result = self._name + ": " + self._description
+        if self._encrypted:
+            result = result + " [Encrypted]"
+        return result
--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 """
 File: main.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Manavjot Singh Dutta
+ID: 110430330
+Username: dutmy005
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
