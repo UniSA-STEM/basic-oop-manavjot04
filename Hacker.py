7e018901b77205c5c5604af454229d776fbd5ec0
manavjot04 <dutmy005@mymail.unisa.edu.au>
2025-10-18 00:18:27 +1030

    Started with the Hacker class

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,13 @@ ID: 110430330
 Username: dutmy005
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from Asset import CryptoToken
+from Rig import Rig
+
+class Hacker:
+    def __init__(self, name):
+        self._name = name
+        self._inventory = [CryptoToken()]
+        self._rig = None
+        self._trace_level = 0
+        self._trace_threshold = 5
